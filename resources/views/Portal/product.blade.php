b71881ad24bd6a5489188ddb6d8a53636795d734
HaLan0101 <dnhalan0101@gmail.com>
2022-05-17 09:59:56 +0200

    fix bug

--- resources/views/Portal/product.blade.php
+++ resources/views/Portal/product.blade.php
@@ -4,17 +4,17 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-                    <p class="mb-4">L'information des produits.</p>
+                    <p class="mb-4">L'information des concerts.</p>
                     <a href="{{url('/createproduct')}}" class="btn btn-primary btn-icon-split btn-sm">
                         <span class="icon text-white-50">
                             <i class="fas fa-plus"></i>
                         </span>
-                        <span class="text">Créer d'un produit</span>
+                        <span class="text">Créer d'un concert</span>
                     </a>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Produits</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Concerts</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
